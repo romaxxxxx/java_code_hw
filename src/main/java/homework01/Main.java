f6f9ff542a1c2cbc0abda6fb9785af9d454c7aa3
Roman Kolbenkov <Kolbenkov@mail.ru>
2022-11-28 20:32:45 +0300

    cyrcels_hw_Kolbenkov_RV

--- src/main/java/homework01/Main.java
+++ src/main/java/homework01/Main.java
@@ -8,49 +8,15 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         //Нахождение минимально присутствующих в массиве чисел
-        int[] mass = {-5, 4, -5, 8, 4, -5, 8, -1};
-        int count = 1;
-        boolean bool = true;
-        boolean bool1 = true;
-        HashMap<Integer, Integer> elCount = new HashMap<>();
-        while (bool) {
-            bool = false;
-            for (int i = 0; i < (mass.length - 2); i++) {
-                if (mass[i] > mass[i + 1]) {
-                    int d = mass[i + 1];
-                    mass[i + 1] = mass[i];
-                    mass[i] = d;
-                    bool = true;
-                }
-            }
-        }
-        while (bool1) {
-            bool1 = false;
-            for (int i = 0; i < (mass.length - 1); i++) {
-                if (mass[i + 1] == -1) {
-                    bool1 = false;
-                    break;
-                } else if (mass[i] == mass[i + 1]) {
-                    elCount.put(mass[i], ++count);
-                } else {
-                    count = 0;
-                    elCount.put(mass[i + 1], ++count);
-                    bool1 = true;
-                }
-            }
-        }
-        int minElCount = Collections.min(elCount.values());
-        System.out.println("//////////Нахождение минимально присутствующих в массиве чисел:");
-        System.out.println("Начальный массив: " + Arrays.toString(mass));
-        System.out.println("Минимально повторяемые элементы в массиве:");
-        for (Map.Entry<Integer, Integer> entry : elCount.entrySet()) {
-            if (entry.getValue().equals(minElCount)) {
-                System.out.println(entry.getKey());
-            }
-        }
+        int[] elemArray = {-5, 4, -5, 8, 4, -5, 8, -1};
+        minArrayEl(elemArray);
 
         //Сортировка по ASCII
         String elemLine = "№яzZ@гвб1а8+!";
+        asciiSort(elemLine);
+    }
+
+    static void asciiSort(String elemLine) {
         int len = elemLine.length();
         char[] charArray = new char[len];
         for (int i = 0; i < elemLine.length(); i++) {
@@ -60,5 +26,47 @@ public class Main {
         System.out.println("///////////Сортировка по ASCII");
         System.out.println("Строка для сортировки:" + " " + elemLine);
         System.out.println("Отсортированный массив по ASCII:" + Arrays.toString(charArray));
+    }
+
+    private static void minArrayEl(int[] elemArray) {
+        int count = 1;
+        boolean bool = true;
+        boolean bool1 = true;
+        HashMap<Integer, Integer> elCount = new HashMap<>();
+        while (bool) {
+            bool = false;
+            for (int i = 0; i < (elemArray.length - 2); i++) {
+                if (elemArray[i] > elemArray[i + 1]) {
+                    int d = elemArray[i + 1];
+                    elemArray[i + 1] = elemArray[i];
+                    elemArray[i] = d;
+                    bool = true;
+                }
+            }
+        }
+        while (bool1) {
+            bool1 = false;
+            for (int i = 0; i < (elemArray.length - 1); i++) {
+                if (elemArray[i + 1] == -1) {
+                    bool1 = false;
+                    break;
+                } else if (elemArray[i] == elemArray[i + 1]) {
+                    elCount.put(elemArray[i], ++count);
+                } else {
+                    count = 0;
+                    elCount.put(elemArray[i + 1], ++count);
+                    bool1 = true;
+                }
+            }
+        }
+        int minElCount = Collections.min(elCount.values());
+        System.out.println("//////////Нахождение минимально присутствующих в массиве чисел:");
+        System.out.println("Начальный массив: " + Arrays.toString(elemArray));
+        System.out.println("Минимально повторяемые элементы в массиве:");
+        for (Map.Entry<Integer, Integer> entry : elCount.entrySet()) {
+            if (entry.getValue().equals(minElCount)) {
+                System.out.println(entry.getKey());
+            }
+        }
     }
 }
